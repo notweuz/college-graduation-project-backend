31c7057072237ad60db93bda4172b77edc65ae8b
notweuz <weuz.main@gmail.com>
2026-03-16 01:53:52 +0400

    feat: base errors

---
+++ internal/errors/errors.go
@@ -0,0 +1,69 @@
+package errors
+
+import "github.com/gofiber/fiber/v3"
+
+type AppError struct {
+	Status  int
+	Message string
+	Reason  string
+}
+
+func (e *AppError) Error() string {
+	return e.Message
+}
+
+func NotFound(message, reason string) *AppError {
+	return &AppError{
+		Status:  fiber.StatusNotFound,
+		Message: message,
+		Reason:  reason,
+	}
+}
+
+func Unauthorized(message, reason string) *AppError {
+	return &AppError{
+		Status:  fiber.StatusUnauthorized,
+		Message: message,
+		Reason:  reason,
+	}
+}
+
+func BadRequest(message, reason string) *AppError {
+	return &AppError{
+		Status:  fiber.StatusBadRequest,
+		Message: message,
+		Reason:  reason,
+	}
+}
+
+func Conflict(message, reason string) *AppError {
+	return &AppError{
+		Status:  fiber.StatusConflict,
+		Message: message,
+		Reason:  reason,
+	}
+}
+
+func InternalServerError(message, reason string) *AppError {
+	return &AppError{
+		Status:  fiber.StatusInternalServerError,
+		Message: message,
+		Reason:  reason,
+	}
+}
+
+func Forbidden(message, reason string) *AppError {
+	return &AppError{
+		Status:  fiber.StatusForbidden,
+		Message: message,
+		Reason:  reason,
+	}
+}
+
+func UnprocessableEntity(message, reason string) *AppError {
+	return &AppError{
+		Status:  fiber.StatusUnprocessableEntity,
+		Message: message,
+		Reason:  reason,
+	}
+}
