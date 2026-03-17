e447948008503c626c97837a0a41b3712af80c27
notweuz <weuz.main@gmail.com>
2026-03-18 00:05:21 +0400

    feat: add user foreign key to booking

--- internal/model/booking.go
+++ internal/model/booking.go
@@ -6,6 +6,8 @@ type Booking struct {
 	ID            uint64    `gorm:"primaryKey;autoIncrement"`
 	HallID        uint64    `gorm:"not null"`
 	Hall          Hall      `gorm:"foreignKey:HallID;references:id"`
+	UserID        uint64    `gorm:"not null"`
+	User          User      `gorm:"foreignKey:UserID;references:id"`
 	StartDateTime time.Time `gorm:"not null"`
 	EndDateTime   time.Time `gorm:"not null"`
 	TotalPrice    float64   `gorm:"not null"`
