15c92062e1dd00dfc970eeb3345da408193fc323
notweuz <weuz.main@gmail.com>
2026-03-31 21:22:35 +0400

    test: add user get by email test

--- internal/service/user_test.go
+++ internal/service/user_test.go
@@ -18,21 +18,19 @@ func TestUserService_FindByID(t *testing.T) {
 	exampleEmail := "test@example.com" // i dunno why I made email as pointer
 	exampleFullName := "Anton"
 	mockUserDatabase.On("FindByID", uint64(1)).Return(&model.User{
-		ID:           1,
-		Email:        &exampleEmail,
-		PasswordHash: "j43325j4h2453jk2354jhk3245hj4352khj5234hkj3542",
-		FullName:     &exampleFullName,
-		Role:         enum.RoleAdmin,
+		ID:       1,
+		Email:    &exampleEmail,
+		FullName: &exampleFullName,
+		Role:     enum.RoleAdmin,
 	}, nil).Once()
 
 	exampleEmail2 := "nastya@yandex.ru"
 	exampleFullName2 := "Nastya"
 	mockUserDatabase.On("FindByID", uint64(2)).Return(&model.User{
-		ID:           2,
-		Email:        &exampleEmail2,
-		PasswordHash: "j43325j4h2453jk2354jhk3245hj4352khj5234hkj3542",
-		FullName:     &exampleFullName2,
-		Role:         enum.RoleClient,
+		ID:       2,
+		Email:    &exampleEmail2,
+		FullName: &exampleFullName2,
+		Role:     enum.RoleClient,
 	}, nil).Once()
 
 	mockUserDatabase.On("FindByID", mock.AnythingOfType("uint64")).
@@ -95,4 +93,34 @@ func TestUserService_UpdateProfile(t *testing.T) {
 	assert.Equal(t, *mockUser.Email, newMail)
 	assert.Equal(t, *mockUser.FullName, newFullName)
 	assert.Equal(t, mockUser.Role, enum.RoleAdmin)
+}
+
+func TestUserService_FindByEmail(t *testing.T) {
+	mockUserDatabase := mocks.NewMockUserDatabase(t)
+
+	email := "xyz@xyz.xyz"
+	fullName := "Test user"
+	mockUser := &model.User{
+		ID:           1,
+		PasswordHash: "",
+		Email:        &email,
+		FullName:     &fullName,
+	}
+
+	mockUserDatabase.On("FindByEmail", "xyz@xyz.xyz").Return(mockUser, nil)
+	mockUserDatabase.On("FindByEmail", mock.AnythingOfType("string")).Return(nil, gorm.ErrRecordNotFound)
+
+	userServiceTest := NewUserService(mockUserDatabase)
+
+	user, err := userServiceTest.FindByEmail("xyz@xyz.xyz")
+
+	assert.NoError(t, err)
+	assert.Equal(t, user.ID, uint64(1))
+	assert.Equal(t, *user.Email, email)
+	assert.Equal(t, *user.FullName, fullName)
+
+	user, err = userServiceTest.FindByEmail("notexist@notexist.notexist")
+
+	assert.ErrorIs(t, err, gorm.ErrRecordNotFound)
+	assert.Nil(t, user)
 }
